5b6f8f78af388ca47d71f894c6cbe501ab7d6f6e
Navi <masimello0001@gmail.com>
2026-05-08 14:39:23 +0900

    feat: 라우팅 설정

--- Week4/homework/src/App.tsx
+++ Week4/homework/src/App.tsx
@@ -1,12 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@emotion/react";
 import GlobalStyle from "./styles/GlobalStyle";
 import theme from "./styles/theme";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
